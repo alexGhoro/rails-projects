d2e5f2b465c5fa61abe3798da0852a2a64229e2d
Alejandro Matute <alexghoro@outlook.com>
2016-01-03 23:26:24 -0430

    Added form a finished the CRUD for the articles, minor changes to the views (edit and delete links)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,10 +14,14 @@ class ArticlesController < ApplicationController
   	@article = Article.new
   end
 
+  #EDIT /articles/:id/edit
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   #POST /articles
   def create
-  	@article = Article.new(title: params[:article][:title], 
-  						   body:  params[:article][:body])
+  	@article = Article.new(article_params)
   	if @article.save
   		redirect_to @article
   	else
@@ -27,6 +31,24 @@ class ArticlesController < ApplicationController
 
   #PUT /articles/:id
   def update
+    # UPDATE
+    # @article.update_attributes({title: 'Nuevo titulo'})
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
   end
+  #DELETE /article/:id
+  def destroy
+  	@article = Article.find(params[:id])
+  	@article.destroy # destroy elimina el objeto de la BD
+  	redirect_to articles_path
+  end
+  private
 
+  def article_params
+    params.require(:article).permit(:title,:body)
+  end
 end

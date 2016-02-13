3eeba2d4416bbc20fd02acc65ad4a356433a8f5f
Alejandro Matute <alexghoro@outlook.com>
2016-02-13 19:25:22 -0430

    add relation many to many between articles categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208203259) do
+ActiveRecord::Schema.define(version: 20160213210909) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 20160208203259) do
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "has_categories", force: true do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "has_categories", ["article_id"], name: "index_has_categories_on_article_id"
+  add_index "has_categories", ["category_id"], name: "index_has_categories_on_category_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
